eb1592dfd8cd8159d23c78b1cd39258327f1799b
Aiden Smith <80472468+aidendotgg@users.noreply.github.com>
2025-01-08 02:15:00 -0500

    Birthday yesterday

--- src/components/sections/index/Hero.tsx
+++ src/components/sections/index/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero({ inView, descRef }: { inView: boolean, descRef: an
               <Typewriter
                 options={{
                   delay: 30,
-                  strings: ["I'm an 18 year old full-stack developer located in New York"],
+                  strings: ["I'm a 19 year old full-stack developer located in New York"],
                   loop: false,
                   autoStart: true,
                   deleteSpeed: 604800000,
