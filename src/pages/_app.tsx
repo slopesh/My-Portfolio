349e3a1ac70715e8db7b3a45798c6ad8abb21d9d
Array <tharwatreyad123@gmail.com>
2025-06-20 03:56:08 +0400

    fix: update site metadata and branding to Array

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,39 @@
 import "@/styles/globals.css";
-import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
+import { useRouter } from 'next/router'
+import { League_Spartan } from 'next/font/google'
+
+const leagueSpartan = League_Spartan({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  let router = useRouter();
-
+  const router = useRouter()
   return (
-    <>
+    <main className={leagueSpartan.className}>
       <NextSeo
-        title={"Aiden.gg"}
-        description={"Aiden's personal site"}
-        canonical={`https://aiden.gg${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`}
-        themeColor={"#2563eb"}
+        title={"Array"}
+        description={"16 year old full-stack developer"}
+        canonical={`https://your-domain.com${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`}
         openGraph={{
-          url: `https://aiden.gg${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`,
-          title: "Aiden.gg",
-          description: "Aiden's personal site",
+          url: `https://your-domain.com${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`,
+          title: "Array",
+          description: "16 year old full-stack developer",
           images: [
             {
-              url: "https://r2.e-z.host/2082d908-7c65-4fc3-b02a-5f50f9141543/lbo1x6wn.png",
-              alt: "Oliver",
+              url: 'https://r2.e-z.host/2082d908-7c65-4fc3-b02a-5f50f9141543/1w9b4ygr.png',
+              width: 1920,
+              height: 1080,
+              alt: 'og image',
             },
           ],
         }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
       />
       <Component {...pageProps} />
-    </>
-  );
+    </main>
+  )
 }
