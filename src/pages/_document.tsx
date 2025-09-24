d214bc3542afbaf34cb4aeb71a9a9f7d9716c95b
Array <tharwatreyad123@gmail.com>
2025-09-25 00:34:22 +0400

    fix: add cache-busting parameter to favicon to force reload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon" />
+      </Head>
       <body>
         <Main />
         <NextScript />
